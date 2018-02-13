6915d5c254ba994e4b658873f1bf4d02fdb2188f
Wade Wegner <wade.wegner@gmail.com>
2018-02-13 06:10:23 -0800

    Updates

--- bin/post-setup.sh
+++ bin/post-setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 TARGET_ORG_ALIAS=${1:-}
+STAGE=${2:-}
 
-sfdx force:user:permset:assign -n dreamhouse -a "$TARGET_ORG_ALIAS"
-sfdx force:data:tree:import -p assets/data/Broker__c-Property__c-plan.json -a "$TARGET_ORG_ALIAS"
+# if it's a review app or ci ...
+if [ "$STAGE" == "" ]; then
+
+  sfdx force:user:permset:assign -n dreamhouse -a "$TARGET_ORG_ALIAS"
+  sfdx force:data:tree:import -p assets/data/Broker__c-Property__c-plan.json -a "$TARGET_ORG_ALIAS"
+
+fi
