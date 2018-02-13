db404ea38c6b8b9494f2aef665f1835f523d8ee3
Wade Wegner <wade.wegner@gmail.com>
2018-02-13 05:36:27 -0800

    Added required settings

---
+++ bin/post-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+TARGET_ORG_ALIAS=${1:-}
+
+sfdx force:user:permset:assign -n dreamhouse -a "$TARGET_ORG_ALIAS"
+sfdx force:data:tree:import -p assets/data/Broker__c-Property__c-plan.json -a "$TARGET_ORG_ALIAS"
